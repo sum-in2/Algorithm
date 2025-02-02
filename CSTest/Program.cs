7c8ff4b1edb83a8361d7bfac35795974ffed5c42
sum_in <fdse1111@naver.com>
2025-02-02 19:35:53 +0900

    a

---
+++ CSTest/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace BaekJoon;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int result = 0;
+        string[] s = Console.ReadLine().Split();
+
+        if (int.TryParse(s[0], out int valueA) && int.TryParse(s[1], out int valueB))
+        {
+            result = valueA - valueB;
+        }
+
+        Console.WriteLine(result);
+    }
+}
