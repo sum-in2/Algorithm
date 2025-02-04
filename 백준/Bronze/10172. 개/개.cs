d1073bf977990b357d14971f6b4d9dce96b8d9b5
sum_in <116086980+sum-in2@users.noreply.github.com>
2025-02-04 20:08:41 +0900

    [Bronze V] Title: 개, Time: 44 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Text;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("|\\_/|");
+            Console.WriteLine("|q p|   /}");
+            Console.WriteLine("( 0 )\"\"\"\\");
+            Console.WriteLine("|\"^\"`    |");
+            Console.WriteLine("||_/=\\\\__|");
+        }
+    }
+}
